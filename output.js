876cbb74ad2d7fa5fd78e5e4c0a38518673582a5
action <action@github.com>
2024-09-12 03:36:22 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Sep 12 2024 03:31:50 GMT+0000 (Coordinated Universal Time)
+//Thu Sep 12 2024 03:36:21 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 require("global-agent/bootstrap");
